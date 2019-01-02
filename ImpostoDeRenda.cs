e8d05623ed573d82f2e09d82f51cb34094eb4f6f
Gabriel Cordeiro <gabrielsilva.ipms@gmail.com>
2019-01-02 18:22:41 -0200

    Fix calculo do imposto de renda

--- ImpostoDeRenda.cs
+++ ImpostoDeRenda.cs
@@ -8,10 +8,10 @@ namespace mentoria
     public decimal Calcula(decimal valor)
     {
       if (valor <= 1903.98M) return 0;
-      else if (valor > 1903.98M && valor <= 2826.65M) return valor * 0.075M + 142.80M;
-      else if (valor > 2826.65M && valor <= 3751.05M) return valor * 0.15M + 335.03M;
-      else if (valor > 3751.05M && valor <= 4664.68M) return valor * 0.225M + 602.96M;
-      return valor * 0.275M + 826.15M;
+      else if (valor > 1903.98M && valor <= 2826.65M) return valor * 0.075M - 142.80M;
+      else if (valor > 2826.65M && valor <= 3751.05M) return valor * 0.15M - 354.80M;
+      else if (valor > 3751.05M && valor <= 4664.68M) return valor * 0.225M - 636.13M;
+      return valor * 0.275M - 869.36M;
     }
   }
 }
